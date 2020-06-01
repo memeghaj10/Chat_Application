02fdb8241e35ede1b810ee9ffff30d28c708f176
Meghaj Singh <49283403+memeghaj10@users.noreply.github.com>
2020-06-01 15:21:40 +0530

    updated and added users list info+functionalities

--- server/users.js
+++ server/users.js
@@ -5,7 +5,7 @@ const addUser =  ({id,name,room}) => {
     room= room.trim().toLowerCase();
 
 
-const existingUser = users.find(user) => user.room == room && user.name == name);
+const existingUser = users.find(user) => user.room === room && user.name === name);
 if(!name||!room) return {error:'Username and room are required'};
 if(existingUser) return {error: 'Username is Taken'};
 
@@ -13,17 +13,17 @@ const user = { id,name,room};
 
 users.push(user);
 
-return user;
+return {user};
 }
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id==id);
+    const index = users.findIndex((user) => user.id===id);
 
     if(index!=-1) return users.splice(index,1)[0];
   }
 
   const getUser=  (id) => users.find((user) => user.id === id);
 
-  const getUsersInRoom = (room) => users.filter((user) -> user.room===room);
+  const getUsersInRoom = (room) => users.filter((user) => user.room===room);
 
   module.exports = {addUser,removeUser,getUser,getUsersInRoom};
