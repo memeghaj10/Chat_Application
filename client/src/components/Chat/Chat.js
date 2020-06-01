969557b70840899e7a5374900f7b316d2492043e
Meghaj Singh <49283403+memeghaj10@users.noreply.github.com>
2020-06-01 15:25:27 +0530

    updated CSS of Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
 
 const Chat = () => {
-	
-	return (
-		<h1>Hello!</h1>
-		)
+	useEffect(()=> {
+		const data = queryString.parse(location,search);
 
+			socket{name,room} = queryString.parse(location.search);
+
+		setName(name);
+		setRoom(room);
+
+	});
+
+	return {
+		<h1>Chat</h1>
+	}
 }
+
+
+export deafult Chat;
